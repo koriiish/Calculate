fc50e41cc02291105b9268efd5a062250d39e06e
Karrie <karinad000@gmail.com>
2023-12-28 16:55:13 +0300

    calculator

---
+++ Calculate/Calculate/Enum.swift
@@ -0,0 +1,16 @@
+//
+//  Enum.swift
+//  Calculate
+//
+//  Created by Карина Дьячина on 28.12.23.
+//
+
+import Foundation
+
+enum Operations {
+    case noAction
+    case addition
+    case substraction
+    case multiplication
+    case division
+}
